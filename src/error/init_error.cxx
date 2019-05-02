1952941cdc1ce73041b2aa073cffbe271fb602f2
Bodie Solomon <bodie@synapsegarden.net>
2019-05-02 17:18:31 -0400

    Move Phoenix graphics fully into header, put Error in a namespace

--- src/error/init_error.cxx
+++ src/error/init_error.cxx
@@ -1,21 +1,18 @@
 #include <sstream>
 #include <string>
 
-#include "SDL.h"
 #include "init_error.hpp"
 
-using namespace err;
+namespace phx_err {
 
-InitError::InitError() : exception() { msg.append(unknown); }
+    InitError::InitError() : exception() { msg.append(unknown); }
 
-InitError::InitError(std::string&& s)
-    : exception(), msg(std::forward<std::string>(s)) {}
+    InitError::InitError(std::string&& s)
+        : exception(), msg(std::forward<std::string>(s)) {}
 
-InitError::InitError(std::stringstream& ss)
-    : exception(), msg(ss.str()) {}
+    InitError::InitError(std::stringstream& ss)
+        : exception(), msg(ss.str()) {}
 
-const char* InitError::what() const noexcept { return msg.c_str(); }
+    const char* InitError::what() const noexcept { return msg.c_str(); }
 
-SDLInitError::SDLInitError() : InitError() {
-    msg.append(SDL_GetError());
-}
+} // namespace phx_err
