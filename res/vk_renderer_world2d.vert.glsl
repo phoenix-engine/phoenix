c90f72ae85128637b915a8c100072f879173888e
Bodie Solomon <bodie@synapsegarden.net>
2019-12-01 16:48:48 -0500

    Update shader resources / build

--- res/vk_renderer_world2d.vert.glsl
+++ res/vk_renderer_world2d.vert.glsl
@@ -3,23 +3,31 @@
 
 layout(location = 0) out vec3 fragColor;
 
-vec2 positions[3] = vec2[](
-	vec2(-1, -1),
-	vec2( 1, -1),
-	vec2(-1,  1)
+vec2 positions[6] = vec2[](
+    // Top-left triangle
+    vec2(-1, -1),
+    vec2( 1, -1),
+    vec2(-1,  1),
+    // Bottom-right triangle
+    vec2( 1,  1),
+    vec2(-1,  1),
+    vec2( 1, -1)
 );
 
-vec3 colors[3] = vec3[](
-	vec3(1.0, 0.0, 0.0),
-	vec3(0.0, 1.0, 0.0),
-	vec3(0.0, 0.0, 1.0)
+vec3 colors[6] = vec3[](
+    vec3(1.0, 0.0, 0.0),
+    vec3(0.0, 1.0, 0.0),
+    vec3(0.0, 0.0, 1.0),
+    vec3(1.0, 0.0, 0.0),
+    vec3(0.0, 0.0, 1.0),
+    vec3(0.0, 1.0, 0.0)
 );
 
 void main() {
-	gl_Position = vec4(
-		positions[gl_VertexIndex],
-		0.0, 1.0
-	);
+    gl_Position = vec4(
+	positions[gl_VertexIndex],
+	0.0, 1.0
+    );
 
-	fragColor = colors[gl_VertexIndex];
+    fragColor = colors[gl_VertexIndex];
 }
