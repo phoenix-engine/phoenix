0019d09f0ad7d2d315c2f631be3ac46782a78904
Bodie Solomon <bodie@synapsegarden.net>
2019-12-10 17:52:44 -0500

    Recompile world2d shaders

--- dep/res/res/world2d_frag_spv_decl.cxx
+++ dep/res/res/world2d_frag_spv_decl.cxx
@@ -1,7 +1,7 @@
 #include "mapper.hpp"
 
 namespace res {
-    const size_t        Mapper::world2d_frag_spv_len = 536;
+    const size_t        Mapper::world2d_frag_spv_len = 1788;
     const unsigned char Mapper::world2d_frag_spv[]   = {
 #include "world2d_frag_spv_real.cxx"
     };
